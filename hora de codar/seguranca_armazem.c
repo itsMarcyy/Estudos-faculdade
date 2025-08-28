e17c413f9c643614ddad3aa40195bf1146466cd6
itsMarcyy <marcella.pinho.brandao4@gmail.com>
2025-08-28 16:32:30 -0300

    Logica nivel novato

---
+++ hora de codar/seguranca_armazem.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    float temperatura, umidade;
+    unsigned int estoque, estoqueMinimo = 1000;
+
+    printf("Digite a temperatura do armazém: ");
+    scanf("%f", &temperatura);
+    printf("Digite a umidade do armazém: ");
+    scanf("%f", &umidade);
+    printf("Digite o estoque atual do armazém: ");
+    scanf("%u", &estoque);
+
+    if(temperatura > 30){
+        printf("Alerta: Temperatura alta no armazém!\n");
+    } else {
+        printf("Temperatura do armazém está dentro do limite.\n");
+    }
+
+    if(umidade > 50){
+        printf("Alerta: Umidade elevada.\n");
+    }else {
+        printf("Umidade do armazém está dentro do limite.\n");
+    }
+
+    if(estoque < estoqueMinimo){
+        printf("Alerta: Estoque abaixo do mínimo!\n");
+    } else {
+        printf("Estoque do armazém está adequado.\n");
+    }
+    return 0;
+}
