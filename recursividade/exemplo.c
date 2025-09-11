eed27ad1f3b27e853ca3aa8a12a3c081d9a638cd
itsMarcyy <marcella.pinho.brandao4@gmail.com>
2025-09-11 16:35:23 -0300

    Aventureiro

---
+++ recursividade/exemplo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void recursivo(int numero){
+    if(numero > 0){
+        printf("%d\n", numero);
+        recursivo(numero - 1);
+    }
+}
+
+int main(){
+    int quantidade = 10;
+    printf("Contagem regressiva:\n");
+    recursivo(quantidade);
+
+    return 0;
+}
